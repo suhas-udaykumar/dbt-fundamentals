93358bbb29b56dcfa047ac210442fa3d066063f6
suhas.udaykumar@gmail.com <suhas.udaykumar@gmail.com>
2025-05-25 18:57:01 +0000

    before sources

--- models/marts/marketing/add-customers-model.sql
+++ models/marts/marketing/add-customers-model.sql
@@ -11,7 +11,7 @@ with
     ),
 
     orders as (
-        select * from {{ ref('stg_jaffle_shop__orders') }}
+        select * from {{ ref('fct_orders') }}
 
     ),
 
@@ -22,7 +22,8 @@ with
 
             min(order_date) as first_order_date,
             max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
+            count(order_id) as number_of_orders,
+            sum(amount) as lifetime_value
 
         from orders
 
@@ -38,7 +39,8 @@ with
             customers.last_name,
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+            customer_orders.lifetime_value
 
         from customers
 
